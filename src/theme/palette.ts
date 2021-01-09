4ffb0dec93b1d3d0ae5a8ea0a43e4ce3085c9f97
bohdan-vasylenko <bohdan.vasylenko@gmail.com>
2021-01-09 18:45:26 +0100

    theme files added

---
+++ src/theme/palette.ts
@@ -0,0 +1,15 @@
+export const palette = {
+  black: '#1d1d1d',
+  white: '#ffffff',
+  darkBlue: '#232635',
+
+  dimmedWhite: '#F2F2F2',
+  offWhite: '#e6e6e6',
+  orange: '#FBA928',
+  orangeDarker: '#EB9918',
+  lightGrey: '#939AA4',
+  lighterGrey: '#CDD4DA',
+  angry: '#dd3333',
+  green: '#43AA8B',
+  yellow: '#F5CB5C',
+};
